e8de384f255abd31975592c68e539c1c36c4f4f4
pcd15 <112518601+pcd15@users.noreply.github.com>
2023-01-15 21:45:15 -0500

    Update GraphProcessor.java

--- src/GraphProcessor.java
+++ src/GraphProcessor.java
@@ -1,74 +1,171 @@
+import java.security.InvalidAlgorithmParameterException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.security.InvalidAlgorithmParameterException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
- * Demonstrates the calculation of shortest paths in the US Highway
- * network, showing the functionality of GraphProcessor and using
- * Visualize
- * @author King David
- * @author Apostle Paul
- * YESSIR we good
+ * Models a weighted graph of latitude-longitude points
+ * and supports various distance and routing operations.
+ * To do: Add your name(s) as additional authors
+ * @author Brandon Fain
+ * @author David Ntim
+ * @author Paul Dilly
  */
+public class GraphProcessor {
 
-//import java.util.*;
+    // Instance Variables (can change, however): 
+    private static Map<Point, Set<Point>> adjacents;
+    private static Map<Integer, Point> pointIndices;
+    //private static Map<String, Point> cities;
 
-public class GraphDemo {
+    /**
+     * Creates and initializes a graph from a source data
+     * file in the .graph format. Should be called
+     * before any other methods work.
+     * @param file a FileInputStream of the .graph file
+     * @throws Exception if file not found or error reading
+     */
 
-    public static Map<String, Point> cities;
-    public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the name of a city followed by its state's abbreviation, separated by a space (e.g., Durham NC): ");
-        String name1 = input.nextLine();
-        System.out.print("Enter the name of another city followed by its state's abbreviation, separated by a space (e.g., Durham NC): ");
-        String name2 = input.nextLine();
-        /*System.out.println("Enter Latitude of City 1: ");
-        double lat1 = input.nextDouble();
-        System.out.println("Enter Longitude of City 1: ");
-        double lon1 = input.nextDouble();
-        System.out.println("Enter Latitude of City 2: ");
-        double lat2 = input.nextDouble();
-        System.out.println("Enter Longitude of City 2: ");
-        double lon2 = input.nextDouble();*/
-        input.close();
-        createCityMap("data/uscities.csv");
-        //Point city1 = new Point(lat1, lon1);
-        //Point city2 = new Point(lat2, lon2);
-        GraphProcessor graph = new GraphProcessor();
-        FileInputStream usa = new FileInputStream("data/usa.graph");
-        graph.initialize(usa); 
-        Point city1 = cities.get(name1);
-        Point city2 = cities.get(name2);
-        long before = System.nanoTime();
-        Point nearest1 = graph.nearestPoint(city1);
-        Point nearest2 = graph.nearestPoint(city2);
-        List<Point> shortestRoute = graph.route(nearest1, nearest2); 
-        double dist = graph.routeDistance(shortestRoute);
-        long after = System.nanoTime();
-        System.out.println("Nearest point to " + name1 + ": " + nearest1);
-        System.out.println("Nearest point to " + name2 + ": " + nearest2);
-        System.out.println("Shortest route from " + nearest1 + " to " + nearest2 + ": " + dist + " mi");
-        System.out.println("Total time to get nearest points, route, and distance: " + ((after - before) / 1000000) + " ms");
-        Visualize vis = new Visualize("data/usa.vis", "images/usa.png");
-        vis.drawRoute(shortestRoute);
+    public void initialize(FileInputStream file) throws Exception {
+        if (file == null) {
+            throw new Exception("Could not read .graph file");
+        }
+        adjacents = new HashMap<>();
+        pointIndices = new HashMap<>();
+        //cities = new HashMap<>();
+        Scanner sc = new Scanner(file);
+        int vertices = sc.nextInt();
+        int edges = sc.nextInt();
+        int counter = 0;
+        while (counter < vertices) {
+            sc.next();
+            double lat = sc.nextDouble();
+            double lon = sc.nextDouble();
+            Point p = new Point(lat, lon);
+            pointIndices.put(counter, p);
+            adjacents.put(p, new HashSet<Point>());
+            counter++;
+        }
+        counter = 0;
+        while (counter < edges) {
+            int point1 = sc.nextInt();
+            int point2 = sc.nextInt();
+            Point one = pointIndices.get(point1);
+            Point two = pointIndices.get(point2);
+            adjacents.get(one).add(two);
+            adjacents.get(two).add(one);
+            if (!sc.hasNextInt() && counter != edges - 1) {
+                sc.nextLine();
+            }
+            counter++;
+        }
+        sc.close();
     }
 
-    public static void createCityMap(String file) throws FileNotFoundException {
-        cities = new HashMap<>();
-        Scanner city = new Scanner(new FileInputStream(new File(file)));
-        while (city.hasNextLine()) {
-            String[] split = city.nextLine().split(",");
-            if (split.length == 4) {
-                cities.put(split[0] + " " + split[1], new Point(Double.parseDouble(split[2]), Double.parseDouble(split[3])));
-            }
-            else {
-                cities.put(split[0].substring(1) + "," + split[1].substring(0, split[1].length() - 1) + " " + split[2], new Point(Double.parseDouble(split[3]), Double.parseDouble(split[4])));
+    /**
+     * Searches for the point in the graph that is closest in
+     * straight-line distance to the parameter point p
+     * @param p A point, not necessarily in the graph
+     * @return The closest point in the graph to p
+     */
+    public Point nearestPoint(Point p) {
+        double min = Double.MAX_VALUE;
+        Point nearMe = p;
+        for (Point pt : adjacents.keySet()) {
+            double dist = p.distance(pt);
+            if (dist < min) {
+                min = dist;
+                nearMe = pt;
             }
         }
+        return nearMe;
+    }
+
+    /**
+     * Calculates the total distance along the route, summing
+     * the distance between the first and the second Points, 
+     * the second and the third, ..., the second to last and
+     * the last. Distance returned in miles.
+     * @param start Beginning point. May or may not be in the graph.
+     * @param end Destination point May or may not be in the graph.
+     * @return The distance to get from start to end
+     */
+    public double routeDistance(List<Point> route) {
+        double retVal = 0.0;
+        for (int i = 0; i < route.size() - 1; i++) {
+            retVal += route.get(i).distance(route.get(i + 1));
+        }
+        return retVal;
+    }
+
+    /**
+     * Checks if input points are part of a connected component
+     * in the graph, that is, can one get from one to the other
+     * only traversing edges in the graph
+     * @param p1 one point
+     * @param p2 another point
+     * @return true if p2 is reachable from p1 (and vice versa)
+     */
+    public boolean connected(Point p1, Point p2) {
+        if (!adjacents.containsKey(p1) || !adjacents.containsKey(p2)) { return false; }
+        Set<Point> visited = new HashSet<>();
+        Stack<Point> s = new Stack<>();
+        s.push(p1);
+        visited.add(p1);
+        while (!s.isEmpty()) {
+            Point current = s.pop();
+            for (Point p : adjacents.get(current)) {
+                if (p.equals(p2)) { return true; }
+                if (!visited.contains(p)) {
+                    visited.add(p);
+                    s.push(p);
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns the shortest path, traversing the graph, that begins at start
+     * and terminates at end, including start and end as the first and last
+     * points in the returned list. If there is no such route, either because
+     * start is not connected to end or because start equals end, throws an
+     * exception.
+     * @param start Beginning point.
+     * @param end Destination point.
+     * @return The shortest path [start, ..., end].
+     * @throws InvalidAlgorithmParameterException if there is no such route, 
+     * either because start is not connected to end or because start equals end.
+     */
+    public List<Point> route(Point start, Point end) throws InvalidAlgorithmParameterException {
+        if (!connected(start, end) || start.equals(end) || !adjacents.containsKey(start) || !adjacents.containsKey(end)) {
+            throw new InvalidAlgorithmParameterException("No path between start and end");
+        }
+        Map<Point, Point> previous = new HashMap<>();
+        Map<Point, Double> distance = new HashMap<>();
+        Comparator<Point> comp = (a, b) -> Double.compare(distance.get(a), distance.get(b));
+        PriorityQueue<Point> pq = new PriorityQueue<>(comp);
+        distance.put(start, (double) 0);
+        pq.add(start);
+        while (!pq.isEmpty()) {
+            Point current = pq.poll();
+            for (Point adj : adjacents.get(current)) {
+                double dist = current.distance(adj) + distance.get(current);
+                if ((!distance.containsKey(adj) || (dist + 0.1 < distance.get(adj)))) {
+                    distance.put(adj, dist);
+                    previous.put(adj, current);
+                    pq.add(adj);
+                }
+            }
+        }
+        List<Point> list = new LinkedList<>();
+        Point p = end;
+        list.add(end);
+        while (!p.equals(start)) {
+            p = previous.get(p);
+            list.add(0, p);
+        }
+        return new ArrayList<>(list);
     }
 }
